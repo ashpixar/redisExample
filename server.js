1694434cc8b5e66254c7a6e0008cbd4c7533ee70
David Ash <davidjash2pixar@gmail.com>
2017-11-30 16:25:24 -0500

    add redi

--- server.js
+++ server.js
@@ -4,3 +4,6 @@ const app = express()
 app.use(express.static('public'))
 
 app.listen(process.env.PORT || 8080, () => console.log('all is ok'));
+
+const redis = require('redis')
+var client = redis.createClient(process.env.REDIS_URL);
