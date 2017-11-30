68b713bbb3b2dfddd1cb58443237370bcc6e91ab
David Ash <davidjash2pixar@gmail.com>
2017-11-30 16:13:29 -0500

    init app 3

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 const express = require('express');
 
-//var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var redis = require('redis');
 
-//var app = express()
-//var client = redis.createClient();
-
-
 const app = express()
-
 app.use(express.static('public'))
+
+var client = redis.createClient();
 
 app.listen(process.env.PORT || 8080, () => console.log('all is ok'))
