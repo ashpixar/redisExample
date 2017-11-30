35f48fd582479bb69d6e324767d597f8f4346b73
David Ash <davidjash2pixar@gmail.com>
2017-11-30 16:11:48 -0500

    init app 1

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
-const express = require('express')
+const express = require('express');
+
+//var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var redis = require('redis');
+
+//var app = express()
+//var client = redis.createClient();
+
+
 const app = express()
 
 app.use(express.static('public'))
