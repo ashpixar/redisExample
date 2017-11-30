a5af670c7402f215a865eb7a29c9a610e4b9eb43
David Ash <davidjash2pixar@gmail.com>
2017-11-30 16:37:25 -0500

    go back to basics

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ app.use(express.static('public'))
 
 app.listen(process.env.PORT || 8080, () => console.log('all is ok'));
 
-const redis = require('redis')
-var client = redis.createClient(process.env.REDIS_URL);
+//const redis = require('redis')
+//var client = redis.createClient(process.env.REDIS_URL);
